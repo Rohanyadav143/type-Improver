ff5f1fbdb678dac341d2fcaf548c47a082781832
Rohanyadav143 <rohn1895@gmail.com>
2025-04-13 17:18:35 +0530

    Fix routing with createHashRouter and RouterProvider

--- src/index.js
+++ src/index.js
@@ -6,15 +6,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from 'react-router-dom'; // Use HashRouter here
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Error404 from './Component/errorPage';
 import ListenPage from './Component/listenPage';
 import WatchPage from './Component/watchPage';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Define your routes
-let allRoutes = [
+const router = createHashRouter([
   {
     path: '/',
     element: <App />
@@ -31,14 +28,13 @@ let allRoutes = [
     path: '*',
     element: <Error404 />
   }
-];
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    {/* Wrap the Router with HashRouter */}
-    <Router basename="/type-Improver">
-      <RouterProvider router={allRoutes} />
-    </Router>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
